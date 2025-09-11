8647542d9d8b05acd73fd36a267826a3816575a5
Ben Navetta <ben@warp.dev>
2025-09-11 16:31:15 +0100

    URL updates

--- Casks/warp-cli.rb
+++ Casks/warp-cli.rb
@@ -1,10 +1,11 @@
 cask "warp-cli" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.2025.09.03.08.11.stable_02"
-  sha256 arm: "02742d107b55aee442acf6fa6a0729733aa53ada0fa8c6b41102a1cb7857af0a", intel: "XXX"
+  version "0.2025.09.11.08.11.dev_00"
+  sha256 arm:   "<placeholder>",
+         intel: "<placeholder>"
 
-  url "https://app.warp.dev/download/cli?os=macos&format=standalone&arch=#{arch}&version=v#{version}"
+  url "https://app.warp.dev/download/cli?os=macos&package=standalone&arch=#{arch}&version=v#{version}"
   name "Warp CLI"
   desc "Command-line interface to Warp agents"
   homepage "https://www.warp.dev/"
@@ -12,9 +13,9 @@ cask "warp-cli" do
   livecheck do
     url "https://releases.warp.dev/channel_versions.json"
     strategy :json do |json|
-      json.dig("dev", "version")&.delete_prefix("v")
+      json.dig("stable", "version")&.delete_prefix("v")
     end
   end
 
-  binary "warp-dev", target: "warp"
+  binary "warp"
 end
--- Casks/warp-cli@preview.rb
+++ Casks/warp-cli@preview.rb
@@ -2,11 +2,12 @@ cask "warp-cli@preview" do
   arch arm: "aarch64", intel: "x86_64"
 
   version "0.2025.09.03.08.11.preview_01"
-  sha256 arm: "02742d107b55aee442acf6fa6a0729733aa53ada0fa8c6b41102a1cb7857af0a", intel: "XXX"
+  sha256 arm:   "<placeholder>",
+         intel: "<placeholder>"
 
-  url "https://app.warp.dev/download/cli?channel=preview&os=macos&format=standalone&arch=#{arch}&version=v#{version}"
+  url "https://app.warp.dev/download/cli?channel=preview&os=macos&package=standalone&arch=#{arch}&version=v#{version}"
   name "Warp CLI (Preview)"
-  desc "Command-line interface to Warp agents (Preview build)"
+  desc "Command-line interface to Warp agents"
   homepage "https://www.warp.dev/"
 
   livecheck do
@@ -16,7 +17,5 @@ cask "warp-cli@preview" do
     end
   end
 
-  container type: :naked
-
-  binary "v#{version}", target: "warp"
+  binary "warp-preview"
 end
