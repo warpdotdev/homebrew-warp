befa8b53c98de00fad8cd1e07366c7b1c1661128
Anka <runner@sjc20-bb660-76a46047-07bd-4e12-a423-075bdea4a32a-CEAD82FBFDAC.local>
2025-10-24 05:59:46 +0000

    warp-cli@preview 0.2025.10.22.08.13.preview_03

--- Casks/warp-cli@preview.rb
+++ Casks/warp-cli@preview.rb
@@ -1,9 +1,9 @@
 cask "warp-cli@preview" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.2025.10.08.08.12.preview_05"
-  sha256 arm:   "cf89a629f6aea665f6c575d166a36bf3813b353f75f2ac64b3702f11a0db56d8",
-         intel: "75fe2aa718a51c603ace182c7076619e6689579a4b9e564cb57b1d102e652c8f"
+  version "0.2025.10.22.08.13.preview_03"
+  sha256 arm:   "b0d56a9d55aefdf389a47e5ab1c21e2f81d086e06d93cee1d09ce814c8d8832d",
+         intel: "d33fae3d1ee127aca543a2fceac4d2092911107c35e80203b6bcb13a77a6fbd8"
 
   url "https://app.warp.dev/download/cli?channel=preview&os=macos&package=tar&arch=#{arch}&version=v#{version}"
   name "Warp CLI (Preview)"
