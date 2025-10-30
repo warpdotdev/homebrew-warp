76bb7b563d5883a65a6f5b70b16eb2a131eace7e
Anka <runner@sjc20-bb714-8c02fce4-844b-4627-830d-7a2f1bfd8bcc-DEC594D1CBAC.local>
2025-10-30 18:45:43 +0000

    warp-cli 0.2025.10.29.08.12.stable_01

--- Casks/warp-cli.rb
+++ Casks/warp-cli.rb
@@ -1,9 +1,9 @@
 cask "warp-cli" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.2025.10.08.08.12.stable_05"
-  sha256 arm:   "9c8a3fa4c023dab10b9cb28977797fd6711cda2a8fcc975c4c1cd3117e5a3002",
-         intel: "5ae7374228ba5ac4dab7e3c1acd462cee64854a51c432ffe918be45ca30fbfa1"
+  version "0.2025.10.29.08.12.stable_01"
+  sha256 arm:   "b2a6cd50a1d0c3fcc99cc3beb6dfbbd804cb8747d3cc5ca7ae9f99b493b6b6e3",
+         intel: "64f2d2e625ed5c7e2745ffd83424ae6e5df75447a4a049d4fd7a040bce486f29"
 
   url "https://app.warp.dev/download/cli?os=macos&package=tar&arch=#{arch}&version=v#{version}"
   name "Warp CLI"
