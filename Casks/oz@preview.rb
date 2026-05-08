744c7f5ae7b19d99fefc449316b2d8b3d7738efa
Anka <runner@sjc20-bb712-6b3fc6cd-45c9-4452-a841-32a10f847906-D24D958E9B8B.local>
2026-05-08 22:52:28 +0000

    oz@preview 0.2026.05.06.15.42.preview_03

--- Casks/oz@preview.rb
+++ Casks/oz@preview.rb
@@ -1,9 +1,9 @@
 cask "oz@preview" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.2026.04.08.08.36.preview_02"
-  sha256 arm:   "4b28282446eba520b657364ebcb166455ccc43a37352ce91c254b66160a44cae",
-         intel: "a11db2c36f94a2e2d0d7b9c9ec6a158944a0b1d9d33a104a463bbcd94063d77b"
+  version "0.2026.05.06.15.42.preview_03"
+  sha256 arm:   "eedcee00440162b1276d29e48c6058680496d3ba9f56a202e890a2792db85a0e",
+         intel: "46496426512f40ffc3523e83c0e39a00197af8ab60c7f6792ac41efe5d29490a"
 
   url "https://app.warp.dev/download/cli?channel=preview&os=macos&package=tar&arch=#{arch}&version=v#{version}"
   name "Oz (Preview)"
