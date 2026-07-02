73765da62503df37144060402ac6df3fc418f3df
github@warp.dev <108495044+warpmachineuser@users.noreply.github.com>
2026-07-01 20:33:16 -0400

    oz 0.2026.06.24.09.19.stable_04 (#180)
    Co-authored-by: Anka <runner@sjc20-bb713-01d2cf0b-3dd5-4bfb-a638-e0aaee609f37-6E4BF3EF81BD.local>
    Co-authored-by: David Stern <vorporeal@gmail.com>

--- Casks/oz.rb
+++ Casks/oz.rb
@@ -1,9 +1,9 @@
 cask "oz" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.2026.06.24.09.19.stable_03"
-  sha256 arm:   "1267934e862d4223d8c3c5686d065be858dac5bd383ec0ff27a3b2a84111b31e",
-         intel: "33f55a4fc2919eae6653218db7af288651cb67cfd2f5872d88013f518fda20e6"
+  version "0.2026.06.24.09.19.stable_04"
+  sha256 arm:   "0ceafb28aead4f6d81e4973a42241bbfe42091dbb0dcccb7e2aa841c53fd7659",
+         intel: "d7df36e3bef5ab1a54a6a9a90307e5b3737f70bd4b1fce4769dc263cb30ec573"
 
   url "https://app.warp.dev/download/cli?os=macos&package=tar&arch=#{arch}&version=v#{version}"
   name "Oz"
