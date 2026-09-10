2880ce11deac05f1dcdc3a6cb9ef9462fd24eac6
Ben Navetta <1683332+bnavetta@users.noreply.github.com>
2026-09-10 13:03:00 +0000

    oz-agent-worker: add 2026-09-09-16-18-52 bottle.

--- Formula/oz-agent-worker.rb
+++ Formula/oz-agent-worker.rb
@@ -12,6 +12,12 @@ class OzAgentWorker < Formula
     strategy :github_latest
   end
 
+  bottle do
+    root_url "https://github.com/warpdotdev/homebrew-warp/releases/download/oz-agent-worker-2026-09-09-16-18-52"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "90ef9ee65fb4f9ff901b990d60e60a38dfb9bd2fd91a018da7ab526644a918cd"
+    sha256 cellar: :any,                 x86_64_linux: "da734588a3ee0b291b9d8efc7637726c11ffde21776a4383262d512ec76ed56c"
+  end
+
   depends_on "go" => :build
 
   def install
