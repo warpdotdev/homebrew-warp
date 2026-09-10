7076c9df8858a45b6fe967ec38f6d4220be77813
github@warp.dev <108495044+warpmachineuser@users.noreply.github.com>
2026-09-10 14:53:37 +0000

    oz-agent-worker: update 2026-09-09-16-18-52 bottle.

--- Formula/oz-agent-worker.rb
+++ Formula/oz-agent-worker.rb
@@ -14,8 +14,8 @@ class OzAgentWorker < Formula
 
   bottle do
     root_url "https://github.com/warpdotdev/homebrew-warp/releases/download/oz-agent-worker-2026-09-09-16-18-52"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "bd7c6b49c66aff61cf9341ed277439f4a46f19b037a9f58b726dd119ca043391"
-    sha256 cellar: :any,                 x86_64_linux: "56bc622965c347a0fe2b72b3b48c32762d49b222f3673dfc7db974c84900dc4d"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "79171ec8dd3cc8d0e43e88debdb9c86815ac3411c0cf173be9e746002dd54485"
+    sha256 cellar: :any,                 x86_64_linux: "b02319853f6b77863096b7023f5d85d4e45ea1949a7c3e161f22735fd2a51ec5"
   end
 
   depends_on "go" => :build
