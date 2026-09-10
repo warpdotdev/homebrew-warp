c5714864a8a29585281f6634bc3b493b57295676
Anka <runner@lvl04-dq150-ccd6fd0b-8750-4a3f-94b8-a7b3c9cbea3c-DEB3415A4924.local>
2026-09-10 21:43:48 +0000

    oz 0.2026.09.09.08.26.stable_02

--- Casks/oz.rb
+++ Casks/oz.rb
@@ -1,9 +1,9 @@
 cask "oz" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.2026.09.02.08.27.stable_02"
-  sha256 arm:   "624334b9ce6c33885afbdad0576a993d2f2429546a96dfdf42e476dec3870550",
-         intel: "55a520916a862a243c4bdbe704f19e5094e4ec9a0fa7f35bb901f0b78a62f764"
+  version "0.2026.09.09.08.26.stable_02"
+  sha256 arm:   "a4a0f80f5515cab99f87de79c8b35c839e5fc11ec583fc4076c0a89c39759a62",
+         intel: "f79077033615a604f6c6616e34b1e32244b7a8c16c3c616cf8e8db959e007a6d"
 
   url "https://app.warp.dev/download/cli?os=macos&package=tar&arch=#{arch}&version=v#{version}"
   name "Oz"
