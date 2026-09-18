b2302e31aec322657a66fa9c05f78671d9448696
Anka <runner@sat12-bq151-1302c833-6030-40f8-ba74-9919c4806dc1-8E01F4894285.local>
2026-09-18 01:23:44 +0000

    warp-agent-cli 0.2026.09.16.08.27.stable_02

--- Casks/warp-agent-cli.rb
+++ Casks/warp-agent-cli.rb
@@ -2,11 +2,11 @@ cask "warp-agent-cli" do
   arch arm: "aarch64", intel: "x86_64"
   os macos: "macos", linux: "linux"
 
-  version "0.2026.09.16.08.27.stable_01"
-  sha256 arm:          "32b43666085a9d2156be1ad492a83134f0d75778bb7d1fca6ab14c12eb84ee32",
-         intel:        "a35eddfea85f65e41a7473e85563bf1d7c44d38229d3f6d474ee1a674a198168",
-         arm64_linux:  "5a21678729b0eef83c7af3f7b3274b3e7970682a189de16fcdc3203d84d46501",
-         x86_64_linux: "6383c8930ba4f5053f0110f70cac3802dda9bc936a7217019da9ff77bfae6e3d"
+  version "0.2026.09.16.08.27.stable_02"
+  sha256 arm:          "06b6594c1cd9f612e976ae97ca0f6041d7124d29858049001bf49172ccd80c6b",
+         intel:        "b0ad445a558073a5cd1e54639a44236938d65df1ac7af44719654764667077f4",
+         arm64_linux:  "37f9717025f0cd3266b18b06703a84ad4175a16eaa2c076fd813bc8788da8a14",
+         x86_64_linux: "2ce34c056ecf7dab33af13983af03139bf6c4bd0041e27bdfad987910fcb4332"
 
   on_macos do
     depends_on macos: :sonoma
